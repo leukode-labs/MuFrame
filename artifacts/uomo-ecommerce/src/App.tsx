bb791c94203e60b7603423872e229b0a2818449a
Leukode Labs <leukodelabs@gmail.com>
2026-04-20 17:21:22 +0200

    Initial commit: MuFrame ecommerce website

---
+++ artifacts/uomo-ecommerce/src/App.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Shop from "./Pages/Shop";
+import Contact from "./Pages/Contact";
+import Blog from "./Pages/Blog";
+import Header from "./Components/Header/Navbar";
+import Footer from "./Components/Footer/Footer";
+import ProductDetails from "./Pages/ProductDetails";
+import NotFound from "./Pages/NotFound";
+import ScrollToTop from "./Components/ScrollButton/ScrollToTop";
+import Authentication from "./Pages/Authentication";
+import ResetPass from "./Components/Authentication/Reset/ResetPass";
+import BlogDetails from "./Components/Blog/BlogDetails/BlogDetails";
+import TermsConditions from "./Pages/TermsConditions";
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
+import Popup from "./Components/PopupBanner/Popup";
+
+const App = () => {
+  return (
+    <>
+      <Popup />
+      <ScrollToTop />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/product" element={<ProductDetails />} />
+          <Route path="/loginSignUp" element={<Authentication />} />
+          <Route path="/resetPassword" element={<ResetPass />} />
+          <Route path="/BlogDetails" element={<BlogDetails />} />
+          <Route path="/terms" element={<TermsConditions />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
